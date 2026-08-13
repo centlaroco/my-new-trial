c9d2122af9ac92809db074d9ad86211cb602aa44
centlaroco <vinentlaroco@gmail.com>
2026-08-13 15:21:48 +0800

    update2

--- src/components/common/aboutMe.tsx
+++ src/components/common/aboutMe.tsx
@@ -19,6 +19,7 @@ export function AboutMe() {
                         className='h-auto w-100 shadow-lg shadow-gray-700/100'
                     />
                 </div>
+
                 <div className='cols-span-2 p-2 text-gray-700 text-lg  leading-loose '>
                     <motion.div initial={{ opacity: -15, y: 1 }} animate={{ opacity: 1, y: 20 }} className='bg-white border border-gray-200 px-5 py-3 rounded-xl shadow-md'>
                         Hi, I’m Vincent Laroco - a passionate and curious individual with a growing interest in the
--- src/components/common/education.tsx
+++ src/components/common/education.tsx
@@ -13,7 +13,7 @@ export function Education() {
                 <h2 className="font-bold text-3xl p-3">Education</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-3">
                     <motion.div className="h-full" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 0.6 }}>
-                        <Card title="Bachelor of Information Technology" schoolDescription="Cordova Public College" buttonLabel="Currently" description="Pursuing a degree in Information Technology with a focus on software development." buttonVariant="primary" logo={CPC} logoAlt="Cordova Public College logo" />
+                        <Card title="Bachelor of Information Technology" schoolDescription="Cordova Public College" buttonLabel="Currently" description="Pursuing a degree in Information Technology." buttonVariant="secondary" logo={CPC} logoAlt="Cordova Public College logo" />
                     </motion.div>
                     <motion.div className="h-full" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 0.7 }}>
                         <Card title="Senior High School" schoolDescription="Babag National High School" buttonLabel="Graduated" description="Completed senior high school with a focus on ICT subjects." buttonVariant="primary" logo={SHS} logoAlt="Babag National High School logo" />
