002716361efae98a3a9069714bfb49948f1130d4
centlaroco <vinentlaroco@gmail.com>
2026-08-14 16:56:30 +0800

    update3

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -34,11 +34,6 @@ export function Footer() {
           <motion.div initial={{ opacity: 0, x: -10 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 0.6, delay: 0.3 }} >
             <NavLink to="/about" className="text-gray-600 hover:text-green-700">About</NavLink>
           </motion.div>
-
-          <motion.div initial={{ opacity: 0, x: -10 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 0.7, delay: 0.4 }} >
-            <NavLink to="/projects" className="text-gray-600 hover:text-green-700">Projects</NavLink>
-
-          </motion.div>
         </div>
 
         <div className="space-y-4">
