40cf0b0fe3ac1c6be56fd4bd4e51440311fe4d96
centlaroco <vinentlaroco@gmail.com>
2026-09-23 23:19:07 +0800

    update the entry point that mounts my portfolio application and defines the main routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,20 @@ import Layout from '@/pages/guest/layyout';
 import Homepage from '@/pages/guest/home';
 import Aboutpage from '@/pages/guest/about';
 import SkillsPage from '@/pages/guest/skills';
+import ProjectPage from '@/pages/guest/projects';
+import NotFoundPage from '@/components/common/notfound'
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route element={<Layout />}>
         <Route path='/' element={<Homepage />} />
         <Route path='/about' element={<Aboutpage />} />
-        <Route path = '/skills' element={<SkillsPage />}/>
-       </Route>
+        <Route path='/skills' element={<SkillsPage />} />
+        <Route path='/project' element={<ProjectPage />} />
+      </Route>
+
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>,
 );
