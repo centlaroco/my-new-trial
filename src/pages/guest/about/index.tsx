316aef91f6ba3953d20997adbde3b69a858b88f6
centlaroco <vinentlaroco@gmail.com>
2026-09-24 19:01:25 +0800

    add about title page

--- src/pages/guest/about/index.tsx
+++ src/pages/guest/about/index.tsx
@@ -1,5 +1,6 @@
 import  {About}  from "@/features/about/about";
 import {useDocumentTitle} from "@/components/ui/title";
+
 export default function AboutT(){
     useDocumentTitle("About | Vincent")
     return ( 
