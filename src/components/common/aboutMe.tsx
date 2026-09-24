3c397674a4f80bf735b06d922142e9303f8beff2
centlaroco <vinentlaroco@gmail.com>
2026-09-24 19:44:39 +0800

    update aboutMe layout

--- src/components/common/aboutMe.tsx
+++ src/components/common/aboutMe.tsx
@@ -5,34 +5,33 @@ import profile from '@/assets/profile2.jpg';
 export function AboutMe() {
     return (
         <section className="max-w-7xl mx-auto lg:px-8 px-4">
-                                <h1 className='font-bold text-3xl md:pt-8'>About me</h1>
+            <h1 className='font-bold text-3xl py-5 '>About Me</h1>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 items-center gap-8'>
-                <div className='cols-span-1 '>
-                    <motion.img
-                        src={profile}
-                        alt="profile yarn"
-                        initial={{ opacity: 0, x: -100 }}
-                        whileInView={{ opacity: 1, x: 0 }}
-                        viewport={{ once: true, amount: 0.3 }}
-                        transition={{ duration: 0.6, ease: 'easeOut' }}
-                        className='h-auto w-100 shadow-lg shadow md:block hidden'
-                    />
-                </div>
+            <div className='grid grid-cols-2 items-center gap-8 '>
 
-                <div className='cols-span-1 text-gray-700 text-lg leading-loose '>
+                <motion.img
+                    src={profile}
+                    alt="profile yarn"
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    transition={{ duration: 0.6, ease: 'easeOut' }}
+                    className='h-auto w-100 shadow-lg shadow md:block hidden'
+                />
+
+                <div className='text-gray-700 text-lg leading-loose '>
                     <motion.div initial={{ opacity: 0, y: 120 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         viewport={{ once: true, amount: 0.3 }}
-                        transition={{ duration: 0.6, ease: 'easeOut' }}className='bg-white border border-gray-200 px-5 py-3 rounded-xl shadow-md'>
+                        transition={{ duration: 0.6, ease: 'easeOut' }} className='bg-white border border-gray-200 px-5 py-6 rounded-xl shadow-md'>
                         Hi, I’m Vincent Laroco - a passionate and curious individual with a growing interest in the
                         world of technology. I am currently a 3rd Year College at Cordova Public College
                         I’m driven by the desire to solve real-world problems through tech. I’m always excited to gain new knowledge and push
                         my skills further every day.
                     </motion.div>
 
-                    <motion.div initial={{ opacity: -5, y: 100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{once:true, amount:0.3}} transition={{ duration: 0.7, ease: 'easeOut' }}>
-                        <Button className="mt-15 w-full">Babag 1, LLC</Button>
+                    <motion.div initial={{ opacity: -5, y: 100 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true, amount: 0.3 }} transition={{ duration: 0.7, ease: 'easeOut' }}>
+                        <Button className="mt-5 w-full">Babag 1, LLC</Button>
                     </motion.div>
                 </div>
             </div>
